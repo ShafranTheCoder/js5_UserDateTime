4797055e98772f7df4be5ea8b740c2a7d230df33
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-04-16 15:29:48 +0300

    Did not hw5

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 'use strict';
 
-createNewUser(){
 
-}
+// let date = new Date();
+// console.log("TCL: date", date)
+
+// function createNewUser() {
+//   let user = {
+//     firstName: prompt("Enter firstName:"),
+//     secondName: prompt("Enter secondtName:"),
+//     birthday: prompt("Enter your birthday(dd.mm.yyyy):"),
+
+//     getAge: function (age) {
+//       return date.getFullYear();
+//     },
+//     getPassword: function () {
+
+//     }
+//   }
+// }
+// createNewUser();
+// createNewUser.getAge();
+
+let date = new Date();
+let birthday = new Date();
+
+
+birthday.setFullYear(1997, 2, 21);
+console.log("TCL: birthday", birthday)
+console.log(date.getFullYear() - birthday);
